cb3a5d422535d9682dde5d17745ee721cba4b907
b-sharman <byron.n.sharman@gmail.com>
2024-10-01 13:52:14 +1300

    a very non-atomic commit
    some of the things I remember changing include:
    * replace int with uint
    * store hands and scores in slices, thereby supporting arbitrary numbers
      thereof
    * implement logic for all the remaining single-player cards

--- hello.go
+++ hello.go
@@ -3,40 +3,61 @@ package main
 import "fmt"
 
 type Nigiri struct {
-	value     int
+	value     uint
 	on_wasabi bool
 }
 
 type Hand struct {
-	dumpling int
-	maki     []int
+	dumpling, pudding, sashimi, tempura uint
+	maki     []uint
 	nigiri   []Nigiri
-	pudding  int
-	sashimi  int
-	tempura  int
 }
 
 func main() {
-	hand := Hand{
-		0,                               // dumpling
-		[]int{},                         // maki
-		[]Nigiri{{2, true}, {3, false}}, // nigiri
-		0,                               // pudding
-		0,                               // sashimi
-		3,                               // tempura
+	hands := []Hand{
+		{
+			0,                               // dumpling
+			2,                               // pudding
+			0,                               // sashimi
+			3,                               // tempura
+			[]uint{3, 3},                    // maki
+			[]Nigiri{{2, true}, {3, false}}, // nigiri
+		},
+		{
+			3,                    // dumpling
+			0,                    // pudding
+			2,                    // sashimi
+			3,                    // tempura
+			[]uint{2, 3, 2},      // maki
+			[]Nigiri{{2, false}}, // nigiri
+		},
 	}
 
-	score := 0
-	score += []int{0, 1, 3, 6, 10, 15}[hand.dumpling]
-	for _, n := range hand.nigiri {
-		if n.on_wasabi {
-			score += 3 * n.value
-		} else {
-			score += n.value
+	scores := []uint{0, 0}
+
+	// types of cards that don't depend on other players
+	for i := range scores {
+		scores[i] += []uint{0, 1, 3, 6, 10, 15}[hands[i].dumpling]
+		for _, n := range hands[i].nigiri {
+			if n.on_wasabi { 
+				scores[i] += 3 * n.value
+			} else {
+				scores[i] += n.value
+			}
 		}
+		scores[i] += 5 * (hands[i].sashimi / 3)
+		scores[i] += 5 * (hands[i].tempura / 2)
 	}
-	score += 5 * (hand.sashimi / 3)
-	score += 5 * (hand.tempura / 2)
 
-	fmt.Println(score)
+	// // types of cards that depend on other players
+	// for i := range scores {
+	//        // award 6 points to the player with the most puddings
+	//        // award 6 points to the player with the most puddings
+    //        // handle ties for both
+	//        f
+	// }
+
+	for _, score := range scores {
+        fmt.Println(score)
+    }
 }
