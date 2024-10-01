52ea8a4254c3e321710521a350614745b1ac4aa3
b-sharman <byron.n.sharman@gmail.com>
2024-10-01 13:01:38 +1300

    another idea

--- hello.go
+++ hello.go
@@ -2,68 +2,60 @@ package main
 
 import "fmt"
 
-// type Card interface {
-// 	Score([]Instance) int
-// }
-
-type (
-	Tempura struct{}
-
-	Dumpling struct{}
-
-	Nigiri struct {
-		value     int // TODO: confine to 1, 2, or 3
-		on_wasabi bool
-	}
-)
-
-/*
-each type of card has a struct
-each of those structs implements Scorable
-Scorable is an interface offering a Score method accepting an array/slice of structs and returning an int
-To calculate the total score of a hand, iterate through all the possible types of cards; for each type, generate a list of the cards in that hand with that type and pass it to the type's Score().
-
-problem: the score of some types of cards (maki rolls, pudding) depends on other players' cards
-*/
+// ooo I'm sure this is so idiomatic
+const CHOPSTICKS = 7
+const DUMPLING   = 2
+const MAKI       = 3
+const NIGIRI     = 4
+const PUDDING    = 5
+const SASHIMI    = 1
+const TEMPURA    = 0
+const WASABI     = 6
 
 func main() {
-	{
-		hand := [3]Nigiri{
-			{1, false},
-			{2, false},
-			{3, true},
-		}
-		sum := 0
-		for _, m := range hand {
-			if m.on_wasabi {
-				sum += 3 * m.value
-			} else {
-				sum += m.value
-			}
-		}
-		fmt.Println("Nigiri:", sum)
+    hand := map[int][]int {
+        CHOPSTICKS: {
+		},
+        DUMPLING: {
+		},
+        MAKI: {
+		},
+        NIGIRI: {
+            2, 3
+		},
+        PUDDING: {
+		},
+        SASHIMI: {
+		},
+        TEMPURA: {
+            0,0,0
+		},
+        WASABI: {
+            0
+		},
+    }
+
+	var wn map[*int]*int // maps wasabi to nigiri
+	wn[&hand[WASABI][0]] = &hand[NIGIRI][0]
+	for _, nigiri := range wn {
+	    nigiri.value *= 3
 	}
 
-	{
-		hand := [5]Tempura{
-			{},
-			{},
-			{},
-			{},
-			{},
-		}
-		sum := 5 * (len(hand) / 2)
-		fmt.Println("Tempura:", sum)
-	}
+    score := 0
+    // for _, value := range DUMPLING {
+    // }
+    // for _, value := range MAKI {
+    // }
+    for _, value := range NIGIRI {
+        score += value
+    }
+    // for _, value := range PUDDING {
+    // }
+    // for _, value := range SASHIMI {
+    // }
+    for _, value := range TEMPURA {
+    }
+    for _, value := range WASABI {
+    }
 
-	{
-		dumpling_scores := [6]int{0, 1, 3, 6, 10, 15}
-		hand := [3]Dumpling{
-			{},
-			{},
-			{},
-		}
-		sum := dumpling_scores[min(4, len(hand))]
-		fmt.Println("Dumpling:", sum)
-	}
 }
