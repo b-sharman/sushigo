03354bb09ebd6980a27e9edfd68215de5453e257
b-sharman <byron.n.sharman@gmail.com>
2024-10-25 16:50:52 +1300

    gofmt ui.go

--- ui/ui.go
+++ ui/ui.go
@@ -9,18 +9,18 @@ import (
 
 func GetNumPlayers() int {
 	stdin := bufio.NewReader(os.Stdin)
-        var num int
-        valid := false
-        for !valid {
-                fmt.Print("Enter the number of players: ")
-                num_results, err := fmt.Scanln(&num)
-                // num_results, err := fmt.Scanf("%d\n", &num)
-                if err == nil && num_results == 1 && num >= MIN_PLAYERS && num <= MAX_PLAYERS {
-                        valid = true
-                }
+	var num int
+	valid := false
+	for !valid {
+		fmt.Print("Enter the number of players: ")
+		num_results, err := fmt.Scanln(&num)
+		// num_results, err := fmt.Scanf("%d\n", &num)
+		if err == nil && num_results == 1 && num >= MIN_PLAYERS && num <= MAX_PLAYERS {
+			valid = true
+		}
 		if num_results != 1 {
 			stdin.ReadString('\n') // clear stdin
 		}
-        }
+	}
 	return num
 }
